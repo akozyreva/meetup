127fa731f25af63959e050374c0d9efe0d1a1ef9
akozyreva <annakozyreva@MacBook-Pro-Anna.local>
2018-12-30 20:45:40 +0100

    add and edit toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <v-app>
-
+  <v-app dark>
+    <v-navigation-drawer temporary fixed v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>trending_up</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon
+              @click.stop="sideNav = !sideNav"
+              class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>trending_up</v-icon>
+          View Meetups</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main></main>
   </v-app>
 </template>
 
@@ -12,7 +35,7 @@ export default {
   },
   data () {
     return {
-      //
+      sideNav: false
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#D32F2F',
+    accent: '#FF5252',
+    secondary: '#BDBDBD',
+    info: '#1E88E5',
+    warning: '#FFA000',
+    error: '#D50000',
+    success: '#66BB6A'
+  }
+})
+
