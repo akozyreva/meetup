522cb6f1226eea8ce6c8462b3bd24f22fcf9fec5
Ваше Имя <anna.ko4523546@gmail.com>
2019-01-01 11:31:12 +0100

    add sign up form

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,10 +1,81 @@
 <template>
-    <p>Sign up</p>
+    <v-container>
+        <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+                            <form>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                        name="email"
+                                        label="Mail"
+                                        id="email"
+                                        v-model="email"
+                                        type="email"
+                                        :rules="['Required']">
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                                name="password"
+                                                label="Password"
+                                                id="password"
+                                                v-model="password"
+                                                type="password"
+                                                :rules="['Required']">
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field
+                                                name="confirmPassword"
+                                                label="Confirm Password"
+                                                id="confirm-password"
+                                                v-model="confirmPassword"
+                                                type="password"
+                                                :rules="[changePassword, 'Required']">
+                                        </v-text-field>
+                                    </v-flex>
+                                </v-layout>
+                                <v-layout>
+                                    <v-flex xs12>
+                                        <v-btn type="submit">Sign up</v-btn>
+                                    </v-flex>
+                                </v-layout>
+                            </form>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: "SignUp"
+        name: "SignUp",
+        data () {
+            return {
+                email: '',
+                password: '',
+                confirmPassword: ''
+            }
+        },
+        computed: {
+            changePassword: function () {
+                return this.password !== this.confirmPassword ? 'Passwords do not match':true
+            }
+        },
+        methods: {
+            onSignUp() {
+
+            }
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,14 +4,23 @@ import App from './App.vue'
 import Vuetify from 'vuetify'
 import router from './router/index.js'
 import {store} from './store/index.js'
-import firebase from 'firebase'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 new Vue({
   router,
   store,
   render: h => h(App),
-}).$mount('#app')
+  created() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBS3QCJTabW3T3Chl76opbOehWWL7PoW-o',
+      authDomain: 'meetup-136a5.firebaseapp.com',
+      databaseURL: 'https://meetup-136a5.firebaseio.com',
+      projectId: 'meetup-136a5',
+      storageBucket: 'meetup-136a5.appspot.com'
+    })
+  }
+}).$mount('#app');
 
 
 Vue.use(Vuetify, {
@@ -24,14 +33,4 @@ Vue.use(Vuetify, {
     error: '#D50000',
     success: '#66BB6A'
   }
-})
-
-var config = {
-  apiKey: "AIzaSyBS3QCJTabW3T3Chl76opbOehWWL7PoW-o",
-  authDomain: "meetup-136a5.firebaseapp.com",
-  databaseURL: "https://meetup-136a5.firebaseio.com",
-  projectId: "meetup-136a5",
-  storageBucket: "meetup-136a5.appspot.com",
-  messagingSenderId: "244766742152"
-};
-firebase.initializeApp(config);
+});
